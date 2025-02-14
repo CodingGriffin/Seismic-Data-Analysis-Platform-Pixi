de00c01b477a0c6acb4815b9e90085b16fa05d63
CodingGriffin <biggriffin640@gmail.com>
2025-02-14 03:31:02 -0600

    Modified GeometryButton and changed styles for modal dialog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,60 @@
-import {useEffect, useState} from 'react'
-import './App.scss'
-// import GeometryButton, {GeometryItem} from "./components/GeometryButton.tsx";
-import { GeometryItem } from './components/GeometryButton.tsx';
-import AddGeometry from './components/AddGeometry.tsx';
+import { useEffect, useState } from "react";
+import "./App.scss";
+import GeometryButton from "./components/GeometryButton.tsx";
+import { GeometryItem } from "./components/GeometryButton.tsx";
+import AddGeometry from "./components/AddGeometry.tsx";
 
 function App() {
-    const [geometry, setGeometry] = useState<GeometryItem[]>([])
-    useEffect(() => {
-        console.log("geometry", geometry)
-    }, [geometry])
-    
-    return (
-        // <GeometryButton geometry={geometry} setGeometry={setGeometry}/>
-        <AddGeometry onSetGeometry={setGeometry} onClose={() => {}}/>
-    )
+  const [geometry, setGeometry] = useState<GeometryItem[]>([]);
+  const [showAddGeometry, setShowAddGeometry] = useState(false);
+  
+  const handleGeometryAdd = (data: { units: string; data: GeometryItem[] }) => {
+    setGeometry(data.data);
+    setShowAddGeometry(false);
+  };
+
+  useEffect(() => {
+    console.log("geometry", geometry);
+  }, [geometry]);
+
+  return (
+    <>
+      <div className="container mt-5">
+        <GeometryButton 
+          geometry={geometry} 
+          addGeometry={() => setShowAddGeometry(true)} 
+          editGeometry={() => {}}
+        />
+      </div>
+      
+      <div 
+        className={`modal-backdrop fade ${showAddGeometry ? 'show' : ''}`} 
+        style={{ display: showAddGeometry ? 'block' : 'none' }}
+      />
+
+      <div 
+        className={`modal fade ${showAddGeometry ? 'show' : ''}`} 
+        style={{ display: showAddGeometry ? 'block' : 'none' }}
+        onClick={(e) => {
+          if (e.target === e.currentTarget) {
+            setShowAddGeometry(false);
+          }
+        }}
+        tabIndex={-1}
+        role="dialog"
+        aria-modal="true"
+      >
+        <div className="modal-dialog">
+          {showAddGeometry && (
+            <AddGeometry
+              onSetGeometry={handleGeometryAdd}
+              onClose={() => setShowAddGeometry(false)}
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
